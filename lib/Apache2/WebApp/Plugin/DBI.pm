e54029979bf37345b614a497fb77bb8a3d6a4528
Marc S. Brooks <mbrooks@cpan.org>
2010-01-05 09:35:47 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-DBI from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-DBI
    gitpan-cpan-version:      
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-DBI.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Apache2/WebApp/Plugin/DBI.pm
+++ lib/Apache2/WebApp/Plugin/DBI.pm
@@ -23,7 +23,7 @@ use base 'Apache2::WebApp::Plugin';
 use DBI;
 use Params::Validate qw( :all );
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
